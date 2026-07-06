1ac4f316434d11dcefde5e47d0e9e9c183baa9c9
wkeyzosa-spec <wkeyzosa@gmail.com>
2026-07-07 00:49:05 +0200

    Modify owners array in config.js
    Updated the owners array to include a specific owner ID.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     secret: "uf-eIwmkz2N0NmQjQbNazt0AmQcW0SlN",
     webhook: "https://discordapp.com/api/webhooks/1523819157441871954/n8jazqkSWOtGaFbjd5o8jN2iH-alWvaI7eKpPLHhaYttnj_0sDipwlWmKmllUnXje4Es",
     prefix: ";",
-    owners: [""],
+    owners: ["1464269311760662745"],
     authLink: "https://discord.com/api/oauth2/authorize?client_id=1519023272635793609&redirect_uri=https://membribottt.onrender.com&response_type=code&scope=identify%20guilds.join",
 
 }
